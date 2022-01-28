e6a66a9dd84497fa247125743c16fbb61f939975
sungm <rlatjdals369@naver.com>
2022-01-28 17:11:48 +0900

    1/28-6

--- app/src/main/java/com/example/chatting/RoomActivity.java
+++ app/src/main/java/com/example/chatting/RoomActivity.java
@@ -30,6 +30,55 @@ public class RoomActivity extends AppCompatActivity {
     EditText et_msg;
     Handler handler = new Handler();
 
+    public class ThreadClass extends Thread{
+        String id;
+
+        ThreadClass(String id) {
+            this.id=id;
+        }
+
+        public void run(){
+            while(true){
+            try{
+                Response.Listener<String> responseListener = new Response.Listener<String>() { //php로 데이터를 보내고 보낸 데이터를 사용하는 부분
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
+                                System.out.println("hongchul" + response);
+
+                                JSONObject jsonObject = new JSONObject(response); //JSON으로 출력된 값을 불러오기
+
+                                JSONArray jsonArray = jsonObject.getJSONArray("result"); //JsonObject JsonArray로 변경
+                                for (int i = 0; i < jsonArray.length(); i++) {
+
+                                    // 로그인에 성공한 경우
+                                    jsonObject = jsonArray.getJSONObject(i);
+
+                                    String userID = jsonObject.getString("userID"); // 그 값중 userID 검색
+                                    String Msg = jsonObject.getString("Msg");
+                                    String Mtime = jsonObject.getString("Mtime");
+
+                                    tv_room.setText(userID + Msg + Mtime);
+
+
+                                }
+                            } catch (Exception e) {
+                                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    };
+                    Room rl = new Room(id, responseListener); //던져줄값 형식 맞춰서 만들어주기
+                    RequestQueue roomqueue = Volley.newRequestQueue(RoomActivity.this); //던져줄값 던질 큐
+                    roomqueue.add(rl);// 큐로 던지기
+                sleep(200);
+
+
+            }catch (Exception e){}
+
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,45 +90,12 @@ public class RoomActivity extends AppCompatActivity {
         Log.d("haha", "onCreate: ");
         long now = System.currentTimeMillis();
 
-
         String id = "ref"+getIntent().getStringExtra("id");
 
-        handler.postDelayed(new Runnable(){
-            public void run(){ //일정시간 딜레이후에 시작
 
-                Response.Listener<String> responseListener = new Response.Listener<String>() { //php로 데이터를 보내고 보낸 데이터를 사용하는 부분
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
-                            System.out.println("hongchul" + response);
+       ThreadClass tc = new ThreadClass(id);
+       tc.start();
 
-                            JSONObject jsonObject = new JSONObject(response); //JSON으로 출력된 값을 불러오기
-
-                            JSONArray jsonArray = jsonObject.getJSONArray("result"); //JsonObject JsonArray로 변경
-                            for(int i = 0 ; i<jsonArray.length(); i++){
-
-                                // 로그인에 성공한 경우
-                                jsonObject = jsonArray.getJSONObject(i);
-
-                                String userID = jsonObject.getString("userID"); // 그 값중 userID 검색
-                                String Msg = jsonObject.getString("Msg");
-                                String Mtime = jsonObject.getString("Mtime");
-
-                                tv_room.setText(tv_room.getText()+userID+Msg+Mtime);
-
-
-                            }
-                        } catch (Exception e) {
-                            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                };
-                Room rl = new Room(id, responseListener); //던져줄값 형식 맞춰서 만들어주기
-                RequestQueue roomqueue = Volley.newRequestQueue(RoomActivity.this); //던져줄값 던질 큐
-                roomqueue.add(rl);// 큐로 던지기
-            }
-        },1000);
 
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,4 +140,5 @@ public class RoomActivity extends AppCompatActivity {
 
         });
     }
+
 }
